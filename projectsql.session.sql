075a0bd0b2ab0388946c7f430b724987cf131fbe
NitishKumarGundapu <nitishkumargundapu@gmail.com>
2021-08-25 11:37:04 +0530

    commit on master

---
+++ projectsql.session.sql
@@ -0,0 +1,8 @@
+/*select * from student;
+
+delete from student where sid = "18521";
+
+insert into student values('bhavesh_kankani','18521','18521','20','24','41');
+*/
+
+insert into student values('bhavesh_kankani','18521','18521','20','24','41');
