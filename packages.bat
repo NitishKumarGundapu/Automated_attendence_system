8cb91fb41c8281665691be52304225152da2ce9d
Nitish Kumar Gundapu <nitishkumargundapu793@gmail.com>
2023-01-10 11:27:15 +0530

    changed the dlib + python v3.9 and executed

--- packages.bat
+++ packages.bat
@@ -1,15 +1,15 @@
-pip install numpy 
+pip install -U numpy 
 
-pip install scipy
+pip install -U scipy
 
-pip install opencv-python
+pip install -U opencv-python
 
-pip install pillow
+pip install -U pillow
 
-pip install mysql-connector
+pip install -U mysql-connector
 
-pip install scikit-learn
+pip install -U scikit-learn
 
-pip install matplotlib
+pip install -U matplotlib
 
-pip install https://pypi.python.org/packages/da/06/bd3e241c4eb0a662914b3b4875fc52dd176a9db0d4a2c915ac2ad8800e9e/dlib-19.7.0-cp36-cp36m-win_amd64.whl#md5=b7330a5b2d46420343fbed5df69e6a3f
+python -m pip install dlib-19.22.99-cp39-cp39-win_amd64.whl
